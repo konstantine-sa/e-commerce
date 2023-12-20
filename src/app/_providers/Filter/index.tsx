57ab5e31cf2afae6e1b0edf0fb7adea04838dd26
konstantine-sa <konstantine.sa@gmail.com>
2023-12-20 19:12:43 +0100

    categories section finished

---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import exp from 'constants'
+import { ReactNode, SetStateAction, createContext, useContext, useState } from 'react'
+
+interface IContextType {
+  categoryFilters: string[]
+  setCategoryFilters: React.Dispatch<SetStateAction<string[]>>
+  sort: string
+  setSort: React.Dispatch<SetStateAction<string>>
+}
+
+export const INITITAL_FILTER_DATA = {
+  categoryFilters: [],
+  setCategoryFilters: () => [],
+  sort: '',
+  setSort: () => '',
+}
+
+const FilterContext = createContext<IContextType>(INITITAL_FILTER_DATA)
+
+export const FilterProvider = ({ children }: { children: ReactNode }) => {
+  const [categoryFilters, setCategoryFilters] = useState([])
+  const [sort, setSort] = useState('-createdAt')
+
+  return (
+    <FilterContext.Provider
+      value={{
+        categoryFilters,
+        setCategoryFilters,
+        sort,
+        setSort,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  )
+}
+
+export const useFilter = () => useContext(FilterContext)
